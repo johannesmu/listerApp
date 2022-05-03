61358903bdad819724de1faa6b5c4c1deccd83e7
johannes <jmuljana@gmail.com>
2022-05-03 18:02:32 +1000

    testing dynamic component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import logo from './logo.svg';
 import { Header } from './components/Header'
 import { Nav  } from './components/Nav';
 
-const NavItems = [
-  {"name" : "Home", "route" : "/"},
-  {"name" : "About", "route" : "/about"}
+const MainNavItems = [
+  {"name" : "Home", "route" : "/", "comp" : "Home"},
+  {"name" : "About", "route" : "/about", "comp" : "About"},
+  {"name" : "Contact", "route" : "/contact", "comp" : "Contact"},
 ]
 
 function App() {
   return (
     <div className="App">
-      <Header name="main" siteName="lister" nav={ <Nav items={NavItems} navClass="main-nav" /> } />
+      <Header name="main" siteName="lister" nav={ <Nav items={MainNavItems} navClass="main-nav" /> } />
     </div>
   );
 }
